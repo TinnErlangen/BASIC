fcafa5646127d5d064628b226bf544827b7e2d32
kimcoco <41998428+kimcoco@users.noreply.github.com>
2021-01-21 12:19:23 +0100

    some fixes; n_jobs=4

--- 01_rawprep.py
+++ 01_rawprep.py
@@ -2,16 +2,14 @@
 import numpy as np
 import mne
 
-# testestestest
-
 # define basic directories for file locations (adjust to your needs)
-base_dir = "D:/XXX_analyses/"
-raw_dir = base_dir+"XXX_raw/"   # where to find the original raw files
-proc_dir = base_dir+"proc/"    # location for saving processed data files
+base_dir = "D:/XXX_data/"
+raw_dir = base_dir+"raw/"   # where to find the original raw files
+proc_dir = "D:/XXX_analyses/proc/"    # location for saving processed data files
 
 # set up your lists for subjects and runs (BTI raw files are saved in runs in separate folders)
 subjs = ["XXX_01","XXX_02",]
-runs = ["1","2","3",]
+runs = ["1","2","3","4"]
 # create new lists, if you want to try things on single subjects or runs
 
 # define frequencies to apply notch filter to (electricity, monitor etc.)
@@ -36,13 +34,12 @@ for sub in subjs:
         for i_idx in range(len(rawmeg_events)):
             if rawmeg_events[i_idx,2]>4000:
                 rawmeg_events[i_idx,2]=rawmeg_events[i_idx,2]-4095
-        # apply the notch filter (remember, we had an online 1.0Hz low-pass filter applied already during the measurement)
+        # apply the notch filter (remember, we had an online 1.0Hz high-pass filter applied already during the measurement)
         picks = mne.pick_types(rawmeg.info, meg=True,ref_meg=True)  # we do it also on ref channels, so select ref_meg=True, (for combined ICA later)
-        rawmeg.notch_filter(notches,picks=picks,n_jobs="cuda",notch_widths=breadths) #cuda is for use of GPU for processing, if not available on your machine, leave out
+        rawmeg.notch_filter(notches,picks=picks,n_jobs=4,notch_widths=breadths) #n_jobs = "cuda": cuda is for use of GPU for processing, if not available on your machine, leave out
         # resample, giving desired sample rate; pass the event list to keep original event time points (!)
-        rawmeg,rawmeg_events = rawmeg.resample(200,events=rawmeg_events,n_jobs="cuda") #see above for cuda
-        meg_events = rawmeg_events
+        rawmeg,rawmeg_events = rawmeg.resample(200,events=rawmeg_events,n_jobs=4) #n_jobs = "cuda": see above for cuda
         # saving the event array as a numpy file
-        np.save(proc_dir+sub+"_"+run+"_events.npy",meg_events)
+        np.save(proc_dir+sub+"_"+run+"_events.npy",rawmeg_events)
         # save the filtered and resampled data as -raw.fif file
         rawmeg.save(proc_dir+sub+"_"+run+"-raw.fif",overwrite=True)
